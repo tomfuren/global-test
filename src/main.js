a6b2ba4133fb2e8144f98159af9def72db4e9312
Tomoki Sakamoto <tsak0007@student.monash.edu>
2025-08-16 23:56:27 +1000

    Add Bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
